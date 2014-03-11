5039e03ea93cc9c1e6614d3e557b5abcbec2ab66
Canzhi Ye <canzhiye@gmail.com>
2014-03-10 19:47:12 -0500

    Initial Commit

---
+++ Video Stream Hack/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Video Stream Hack
+//
+//  Created by Canzhi Ye on 3/10/14.
+//  Copyright (c) 2014 Canzhi Ye. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
