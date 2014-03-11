be63c927bd6c930fd65dcb41c4df07025665b355
Canzhi Ye <canzhiye@gmail.com>
2014-03-10 20:44:48 -0500

    initial commit

---
+++ iOS/Broadcasts/Constants.h
@@ -0,0 +1,8 @@
+// Fill in your own keys and then rename this file to Constants.h
+
+// OpenTok API Key
+static NSString* const kOpentokApiKey = @"44685512";
+
+// Parse Application ID and Key
+static NSString* const kParseAppId = @"xLgOInnG1MOAQCIu3Lvfq9SDcPU4sVcJXv5kzLrz";
+static NSString* const kParseClientKey = @"hSUpZIMF29SErz8W8TRwzQOV6CQClmWM2CzNfsu7";
